39cbfd0d528cdc72daa1f2c1b5a2ea86f2e882f0
Shubham Saxena <shubh.lpu16@gmail.com>
2020-07-29 18:53:30 +0530

    lint check

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -5,7 +5,7 @@ import s from './Home.module.css';
 
 const Home = () => {
   const { store } = useContext(Context);
-  console.log(store);
+  console.log(store); //eslint-disable-line
   return (
     <div className={s.App}>
       <header className={s.header}>
