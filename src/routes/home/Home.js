243b6fabcb38d23d68fcda47ee40470b3486aff0
Shubham Saxena <shubh.lpu16@gmail.com>
2020-07-29 18:12:08 +0530

    css modules

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -1,14 +1,15 @@
 import React, { useContext } from 'react';
+import CSSModules from 'react-css-modules';
 import { Context } from '../../AppContext';
-import './Home.css';
+import s from './Home.module.css';
 
 const Home = () => {
   const { store } = useContext(Context);
   console.log(store); //eslint-disable-line
   return (
-    <div className="App">
-      <header className="header">
-        <img src="/images/logo.svg" className="logo" alt="logo" />
+    <div className={s.App}>
+      <header className={s.header}>
+        <img src="/images/logo.svg" className={s.logo} alt="logo" />
         <p>
           A simple React Setup with routes in <code>/src/App.js</code>.<br />
           Redux is also setup with different directories of actions, reducers,
@@ -19,4 +20,4 @@ const Home = () => {
   );
 };
 
-export default Home;
+export default CSSModules(Home, s, { allowMultiple: true });
