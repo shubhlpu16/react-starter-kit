9529b9c0b299a8ea8de97f9996159fca082b1a35
shubh.lpu16@gmail.com <shubh.lpu16@gmail.com>
2021-06-10 22:28:52 +0530

    settings update

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import 'bootstrap/dist/css/bootstrap.css'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider as ReduxProvider } from 'react-redux'
 import { ContextProvider } from './AppContext'
 import store from './store/index'
 // Put any other imports below so that CSS from your
 // components takes precedence over default styles.
-import './index.css'
 import App from './App'
+import './index.css'
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
